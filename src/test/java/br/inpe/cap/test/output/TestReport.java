7d62361a2a9153a9bce73a72f815324a2690ae37
phillima <phyllipe_slf@yahoo.com.br>
2021-03-20 14:56:08 -0300

    Prepare for release 2.4.0. XML report is removed

--- src/test/java/br/inpe/cap/test/output/TestReport.java
+++ src/test/java/br/inpe/cap/test/output/TestReport.java
@@ -14,7 +14,6 @@ import br.inpe.cap.asniffer.AM;
 import br.inpe.cap.asniffer.exceptions.ReportTypeException;
 import br.inpe.cap.asniffer.model.AMReport;
 import br.inpe.cap.asniffer.output.IReport;
-import br.inpe.cap.asniffer.output.xml.XMLReport;
 import br.inpe.cap.asniffer.parameters.ParamMapper;
 import br.inpe.cap.asniffer.parameters.ParameterReadingException;
 import br.inpe.cap.asniffer.parameters.Parameters;
@@ -27,29 +26,6 @@ public class TestReport {
 	private static IReport outputProcessor;
 	static String testFilePath;
 	
-	//@BeforeClass
-	public static void setUp() {
-		testFilePath = System.getProperty("user.dir") + "/annotationtest";
-		report = new AM().calculate(testFilePath, "project");
-		outputProcessor = new XMLReport();
-	}
-		
-	//@Test	
-	public void firstTest() {
-		outputProcessor.generateReport(report, "annotationtest");
-		
-		String expected = null, actual = null;
-		try {
-			expected = new String(Files.readAllBytes(Paths.get(testFilePath + 
-													File.separatorChar + "expected.xml")), StandardCharsets.UTF_8);
-			actual = new String(Files.readAllBytes(Paths.get(testFilePath + 
-													File.separatorChar +  "project.xml")), StandardCharsets.UTF_8);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		assertEquals(expected,actual);
-	}
-	
 	@Test
 	public void readReportType() {
 		
@@ -57,9 +33,6 @@ public class TestReport {
 		String expected = "br.inpe.cap.asniffer.output.json.JSONReport";
 		
 		assertEquals(expected, actualClassName);
-		
-		actualClassName = PropertiesUtil.getReportType("xml");
-		expected = "br.inpe.cap.asniffer.output.xml.XMLReport";
 		
 		assertEquals(expected, actualClassName);
 		
