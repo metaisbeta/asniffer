c1752113a6d098927d1683eb0c71f87532f14e75
Felipe Mandu Pereira <fmandu@pagseguro.com>
2021-10-25 19:22:36 -0300

    poc: code review

--- src/main/java/com/github/phillima/asniffer/ClassInfo.java
+++ src/main/java/com/github/phillima/asniffer/ClassInfo.java
@@ -115,7 +115,8 @@ public class ClassInfo extends VoidVisitorAdapter<Object> {
                     .ifPresent(
                             typeDeclaration -> {
                                 defineTypeInClassInfo(typeDeclaration);
-                                packageName = cu.getPackageDeclaration().get().getNameAsString();
+                                cu.getPackageDeclaration().ifPresent(packageDeclaration ->
+                                        packageName = packageDeclaration.getNameAsString());
                                 className = generateClassName();
                             }
                     );
