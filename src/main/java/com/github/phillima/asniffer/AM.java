f52a1e355a0c68f66eed5731b53860e00ab231ee
Maik Henrique <maik.hsp.contato@gmail.com>
2021-10-26 20:32:55 -0300

    fix: correction of merge issues

--- src/main/java/com/github/phillima/asniffer/AM.java
+++ src/main/java/com/github/phillima/asniffer/AM.java
@@ -25,7 +25,6 @@ import java.util.stream.Collectors;
 
 public class AM {
 
-<<<<<<< HEAD
     public AMReport calculate(String path, String projectName) {
         String[] srcDirs = FileUtils.getAllDirs(path);
         String[] javaFiles = FileUtils.getAllJavaFiles(path);
@@ -77,77 +76,5 @@ public class AM {
 
         return metrics;
     }
-=======
-	private static final int MAX_AT_ONCE;
-
-	static {
-		String jdtMax = System.getProperty("jdt.max");
-		if(jdtMax!=null) {
-			MAX_AT_ONCE = Integer.parseInt(jdtMax);
-		} else {
-			long maxMemory= Runtime.getRuntime().maxMemory() / (1 << 20); // in MiB
-            
-			
-			if      (maxMemory >= 2000) MAX_AT_ONCE= 400;
-			else if (maxMemory >= 1500) MAX_AT_ONCE= 300;
-			else if (maxMemory >= 1000) MAX_AT_ONCE= 200;
-			else if (maxMemory >=  500) MAX_AT_ONCE= 100;
-			else                        MAX_AT_ONCE=  25;
-		}
-	}
-
-	public AMReport calculate(String path, String projectName) {
-		String[] srcDirs = FileUtils.getAllDirs(path);
-		String[] javaFiles = FileUtils.getAllJavaFiles(path);
-		
-		MetricsExecutor storage = new MetricsExecutor(() -> includeClassMetrics(), 
-						includeAnnotationMetrics(),
-						includeCodeElementMetrics() , projectName);
-		List<List<String>> partitions = Lists.partition(Arrays.asList(javaFiles), MAX_AT_ONCE);
-
-		for(List<String> partition : partitions) {
-			ASTParser parser = ASTParser.newParser(AST.JLS14);
-			
-			Map<String, String> options = JavaCore.getOptions();
-			JavaCore.setComplianceOptions(JavaCore.VERSION_14, options);
-			parser.setCompilerOptions(options);
-			parser.setEnvironment(null, srcDirs, null, true);
-			parser.setResolveBindings(true);
-			parser.setBindingsRecovery(true);
-			parser.createASTs(partition.toArray(new String[partition.size()]), null, new String[0], storage, null);
-		}
-		
-		return storage.getReport();
-	}
-	
-	private List<IClassMetricCollector> includeClassMetrics(){
-		
-		List<IClassMetricCollector> metrics = new ArrayList<>();
-		metrics.add(new AC());
-		metrics.add(new UAC());
-		metrics.add(new ASC());
-		metrics.add(new NAEC());
-
-    	return metrics;
-	}
-	
-	private List<IAnnotationMetricCollector> includeAnnotationMetrics(){
-		
-		List<IAnnotationMetricCollector> metrics = new ArrayList<>();
-		metrics.add(new AA());
-		metrics.add(new ANL());
-		metrics.add(new LOCAD());
-
-		return metrics;
-	}
-	
-	private List<ICodeElementMetricCollector> includeCodeElementMetrics(){
-			
-		List<ICodeElementMetricCollector> metrics = new ArrayList<>();
-		metrics.add(new AED());
-		
-		return metrics;
-	}
->>>>>>> 5bc214c3ea34520d01f904ec7fa2ddd94cc66b07
 
 }
