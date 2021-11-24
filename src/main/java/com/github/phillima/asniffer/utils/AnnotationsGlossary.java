4668af4e85de0c15b5749dd5daeea80a74a82c22
Bruno S Rodrigues <34436334+brusangues@users.noreply.github.com>
2021-11-24 20:52:24 -0300

    fix: json file is now included in jar
    - JPA added to glossary
    - glossary moved to resources
    - gitignore now ignores .factorypath
    
    Co-Authored-By: Maik <48337058+MaikHenriqueSP@users.noreply.github.com>
    Co-Authored-By: Felipe Mandu Pereira <felipemandupereira@outlook.com>
    Co-Authored-By: Bruno S Rodrigues <34436334+brusangues@users.noreply.github.com>
    Co-Authored-By: RafawsMasm <41874395+RafawsMasm@users.noreply.github.com>

--- src/main/java/com/github/phillima/asniffer/utils/AnnotationsGlossary.java
+++ src/main/java/com/github/phillima/asniffer/utils/AnnotationsGlossary.java
@@ -5,22 +5,23 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.nio.file.Paths;
-import java.nio.file.Files;
 import java.io.BufferedReader;
-import com.google.gson.stream.JsonReader;
+import java.io.InputStreamReader;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 public class AnnotationsGlossary {
-    public static String file = "./src/main/java/com/github/phillima/asniffer/utils/Glossary.json";
+    public static String file = "/Glossary.json";
 
     private static Map<String, Set<String>> ANNOTATION_NAME_TO_SCHEMA;
 
     private static void loadMap(){
-        try (BufferedReader fileReader = Files.newBufferedReader(Paths.get(file));){            
+        var stream = new InputStreamReader(AnnotationsGlossary.class.getResourceAsStream(file));
+        try (BufferedReader fileReader = new BufferedReader(stream)){            
             Gson gson = new Gson();
-            ANNOTATION_NAME_TO_SCHEMA = gson.fromJson(fileReader, new TypeToken<HashMap<String, HashSet<String>>>(){}.getType());
+            var type = new TypeToken<HashMap<String, HashSet<String>>>(){}.getType();
+            ANNOTATION_NAME_TO_SCHEMA = gson.fromJson(fileReader, type);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -32,25 +33,6 @@ public class AnnotationsGlossary {
         }
         return Optional.ofNullable(ANNOTATION_NAME_TO_SCHEMA.get(key));
         
-    }
-
-    public static String get_(String key){
-        try (BufferedReader fileReader = Files.newBufferedReader(Paths.get(file));
-            JsonReader reader = new JsonReader(fileReader)) {
-                        
-            reader.beginObject();
-            while (reader.hasNext()) {
-                if(reader.nextName() == key){
-                    return reader.nextString();
-                }
-            }
-            reader.endObject();
-
-            fileReader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
 }
